7ef99fc75a83c2821b1095073f4539bc83f8c1a9
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-29 09:10:06 -0400

    renders pokemon to page, has onclick for backs of sprites, styles header in css, troubleshoots render weirdness

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+  const [showFront, setShowFront] = useState(true);
+  const { name, hp, sprites } = pokemon;
+  const handleClick = () => setShowFront(!showFront);
+  const pokeImage = showFront ? sprites.front : sprites.back;
+  const pokeAlt = pokeImage ? name : "oh no!";
+
   return (
     <Card>
-      <div>
+      <div onClick={handleClick}>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={pokeImage} alt={pokeAlt} />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
